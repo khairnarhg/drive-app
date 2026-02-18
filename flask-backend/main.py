dda72497e376a0d860f1d5826c88313ef04c5060
khairnarhg <hgkhairnar255@outlook.com>
2026-02-18 15:20:11 +0530

    final

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app, db
 
 app = create_app()
 
 if __name__ == "__main__":
-      
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    # Disable debug in production
+    app.run(host="0.0.0.0", port=port, debug=False)
